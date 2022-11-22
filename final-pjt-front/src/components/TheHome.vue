0748a27883cce59d5ce02d43ef3b70cafd06304f
dawnmorning <notify9637@naver.com>
2022-11-23 01:20:52 +0900

    movie card profile recommend design

--- final-pjt-front/src/components/TheHome.vue
+++ final-pjt-front/src/components/TheHome.vue
@@ -1,13 +1,20 @@
 <template>
-  <div>
+  <div style='position:realted'>
     <!-- 로그인 시 보일 화면 -->
-    <div v-if='isLogin'>
-      <div class='jua animate__animated animate__backInUp delay-4s'>
+    <div v-if='isLogin' >
+      <div class='jua animate__animated animate__backInUp delay-4s' style='margin-top:150px;'>
         <div class=''>안녕하세요, {{ nickname }} 님</div> 
         <div class='mt-5 ' >오늘 기분은 어때요?</div>
         <b-button @click='goRecommend' class='btn btn-info btn-lg'><span>영화를 고르러 가볼까요?</span> </b-button>
       </div>
-        
+      <div>
+      <div class="today jua " style='display:inline; position:related; right:1200px;'>
+          <div class='visitor animate__animated animate__heartBeat animate__repeat-3 delay-4s'>
+              <p>오늘 방문자 수</p>
+              <number class="number" :from="0" :to="1125" :duration="6"/>
+          </div>
+      </div>
+      </div>
         <!-- 라우터링크에 애니메이션 넣기 -->
         <!-- <router-link :to="{name: 'RecomView'}">영화를 추천해줄게요!</router-link>
         <b-nav>
@@ -80,7 +87,30 @@ nav{
   right: 0;
 }
 /* 12 */
+.today {
+    margin: 50px auto;
+    text-align: center;
+}
 
+.today p {
+    font-size: 50px;
+    line-height: 1;
+}
+
+.visitor {
+    margin: 30px 0 0 0;
+    padding: 30px;
+    font-size: 40px;
+    font-weight: normal;
+    color:#C613F2;
+}
+
+.number {
+    letter-spacing: 2px;
+    background: white;
+    padding: 10px 20px;
+    border-radius: 20px;
+}
 
 
 /* 애니메이션 */
--- final-pjt-front/src/views/testView.vue
+++ final-pjt-front/src/views/testView.vue
@@ -1,13 +1,20 @@
 <template>
-  
-  <div style='margin-top:200px;'>
-    
+  <div style='margin-top:400px;'>
+    <star-rating id=setstar :star-size="30" v-model="rating" :border-width="5" border-color="#d8d8d8" 
+    :rounded-corners="true" 
+    :star-points="[23,2, 14,17, 0,19, 10,34, 7,50, 23,43, 38,50, 36,34, 46,19, 31,17]">
+    </star-rating>
+
   </div>
 </template>
 
 <script>
+import StarRating from 'vue-star-rating'
 export default {
-  name:'textView'
+  name:'textView',
+  components:{
+    StarRating
+  }
 }
 </script>
 
