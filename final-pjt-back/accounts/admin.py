cc6c569962432609d29d37d825af06ccf91abaf9
김종혁 <notify9637@naver.com>
2022-12-25 17:00:37 +0900

    to public

--- final-pjt-back/accounts/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin  # 기존에 사용하는 User 관리 interface 설정
-from .models import User  # 새롭게 등록한 User 모델
-
-# Register your models here.
-admin.site.register(User, UserAdmin)
--- final-pjt-back/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-# api/v1/
-urlpatterns = [
-    path('<str:username>/', views.user_ru, name='user_ru'),
-    path('follow/<str:username>/', views.follow, name='follow'),
-    path('users/all/', views.users_r, name='users_r'),
-]
