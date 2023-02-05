79efaf7b7d9e46629151e2866d039e6b76919c3c
김종혁 <notify9637@naver.com>
2023-02-05 11:53:16 +0900

    readme작성

--- final-pjt-front/src/components/MovieCard.vue
+++ final-pjt-front/src/components/MovieCard.vue
@@ -1,15 +1,22 @@
 <template>
-	<div v-if="movie">
-		<div>	
-			<img :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`" class='gridimage animate__animated animate__jackInTheBox'>
-			<div class='hoverbtn animate__animated animate__fadeInUp' style=''>
-				<button @click="goPostReview" class='w-btn w-btn-blue fun-btn'><i class="fa-sharp fa-solid fa-question"></i></button>
-				<button @click="likeMovie" class= 'fun-btn '>
-					<span v-if="!isLike"><i class="fa-regular fa-heart"></i></span>
-					<span v-if="isLike"><i class="fa-solid fa-heart"></i></span>
-				</button>
-				<span class='showlikeuser' style='color:white;'>{{like_users_count}}</span>
-				<!-- <div v-if="like_users">
+  <div v-if="movie">
+    <div>
+      <img
+        :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"
+        class="gridimage animate__animated animate__jackInTheBox"
+      />
+      <div class="hoverbtn animate__animated animate__fadeInUp" style="">
+        <button @click="goPostReview" class="w-btn w-btn-blue fun-btn">
+          <i class="fa-sharp fa-solid fa-question"></i>
+        </button>
+        <button @click="likeMovie" class="fun-btn">
+          <span v-if="!isLike"><i class="fa-regular fa-heart"></i></span>
+          <span v-if="isLike"><i class="fa-solid fa-heart"></i></span>
+        </button>
+        <span class="showlikeuser" style="color: white">{{
+          like_users_count
+        }}</span>
+        <!-- <div v-if="like_users">
 					<h6>좋아요 누른 사람</h6>
 					<ul v-for="like_user in like_users" :key="like_user.id">
 							<img :src="`http://127.0.0.1:8000${like_user.profile_image}`" alt="프로필 이미지">
@@ -18,189 +25,193 @@
 						</router-link>
 					</ul>
 				</div> -->
-			</div>
-		</div>
-	</div>                
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
-const DJANGO_URL='http://127.0.0.1:8000'
+import axios from "axios";
+const DJANGO_URL = "http://127.0.0.1:8000";
 
 export default {
-    name:'MovieCard',
-    components:{
-        // Carousel,
-        // Slide
+  name: "MovieCard",
+  components: {
+    // Carousel,
+    // Slide
+  },
+  props: {
+    // movie_id:Number,
+    movie: Object,
+  },
+  data() {
+    return {
+      user_id: this.$store.state.user_id,
+      // movie: null,
+      like_users_count: this.movie.cnt_like_users,
+      like_users: this.movie.like_users,
+    };
+    // is_focus: false,  blur/focus 함수 할때 써먹을용 삭제 x -종혁
+  },
+  computed: {
+    isLike() {
+      return this.like_users.some((user) => {
+        return user.id === this.user_id;
+      });
     },
-    props: {
-        // movie_id:Number,
-        movie:Object,
+  },
+  methods: {
+    goPostReview() {
+      this.$router.push({
+        name: "PostReview",
+        params: { movie_id: this.movie.id, movie: this.movie },
+      });
     },
-    data() {
-        return {
-            user_id: this.$store.state.user_id,
-            // movie: null,
-            like_users_count: this.movie.cnt_like_users,
-            like_users: this.movie.like_users,
-					}
-					// is_focus: false,  blur/focus 함수 할때 써먹을용 삭제 x -종혁
-				},
-		computed: {
-			isLike() {
-				return this.like_users.some(user => { return user.id === this.user_id })
-			},
-		},
-    methods: {
-        goPostReview() {
-            this.$router.push({ name: 'PostReview', params:{ movie_id: this.movie.id, movie: this.movie}})
-				},
-        likeMovie() {
-            axios({
-                method: 'put',
-                url: `${DJANGO_URL}/api/v2/movies/like/${this.movie.id}/`,
-                headers: { Authorization : `Token ${this.$store.state.token}` },
-            })
-            .then(res => {
-                this.isLike = res.data.isLike,
-                this.like_users_count = res.data.count
-                this.like_users = res.data.like_users
-            })
-        },
-        getMovie() {
-            axios({
-                method: 'get',
-                url: `${DJANGO_URL}/api/v2/${this.movie_id}/movies/`,
-                headers: { Authorization : `Token ${this.$store.state.token}` }
-            })
-            .then(res => {
-                const data = res.data
-                // this.movie = res.data
-                this.isLike = data.like_users.some(user => { return user.id === this.user_id })
-                this.like_users_count = data.cnt_like_users
-                this.like_users = data.like_users       
-            })
-            .catch(err => { console.log(err) })
-        }
+    likeMovie() {
+      axios({
+        method: "put",
+        url: `${DJANGO_URL}/api/v2/movies/like/${this.movie.id}/`,
+        headers: { Authorization: `Token ${this.$store.state.token}` },
+      }).then((res) => {
+        (this.isLike = res.data.isLike),
+          (this.like_users_count = res.data.count);
+        this.like_users = res.data.like_users;
+      });
     },
-    // created(){
-    //     this.getMovie()
-    // }
-}
+    getMovie() {
+      axios({
+        method: "get",
+        url: `${DJANGO_URL}/api/v2/${this.movie_id}/movies/`,
+        headers: { Authorization: `Token ${this.$store.state.token}` },
+      })
+        .then((res) => {
+          const data = res.data;
+          // this.movie = res.data
+          this.isLike = data.like_users.some((user) => {
+            return user.id === this.user_id;
+          });
+          this.like_users_count = data.cnt_like_users;
+          this.like_users = data.like_users;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+  // created(){
+  //     this.getMovie()
+  // }
+};
 </script>
 
 <style>
-
-.gridimage{
-		object-fit: cover;
-		height: 750px;
-		/* margin-bottom: -40px; */
-		margin-top: 30px;
-		width: 246px;
-		height: 355px;
+.gridimage {
+  object-fit: cover;
+  height: 750px;
+  /* margin-bottom: -40px; */
+  margin-top: 30px;
+  width: 246px;
+  height: 355px;
 }
-
 
 .color-bg-start {
-	background-color: salmon;
+  background-color: salmon;
 }
-.hoverbtn{
-		cursor: pointer;
-		display: none;
-		text-align: center;
-		/* align-items: center; */
-		margin-top: -30px;
+.hoverbtn {
+  cursor: pointer;
+  display: none;
+  text-align: center;
+  /* align-items: center; */
+  margin-top: -30px;
 }
-img:hover +.hoverbtn{
-		display: block;
+img:hover + .hoverbtn {
+  display: block;
 }
-.hoverbtn:active{
-		display:block;
-		cursor: pointer;
+.hoverbtn:active {
+  display: block;
+  cursor: pointer;
 }
 
 /* toggle class bg-animate-color */
 
 .bg-animate-color {
-	animation: random-bg .5s linear infinite;
+  animation: random-bg 0.5s linear infinite;
 }
-
 
 /* add animation to bg color  */
 
 @keyframes random-bg {
-	from {
-		filter: hue-rotate(0);
-	}
-	to {
-		filter: hue-rotate(360deg);
-	}
+  from {
+    filter: hue-rotate(0);
+  }
+  to {
+    filter: hue-rotate(360deg);
+  }
 }
 
 .fun-btn {
-	/* change bg color to get different hues    */
-	background-color: salmon;
-	color: white;
-	/* padding: 2em 3em; */
-	border: none;
-	transition: all .3s ease;
-	border-radius: 5px;
-	letter-spacing: 2px;
-	text-transform: uppercase;
-	outline: none;
-	align-self: center;
-	cursor: pointer;
-	font-weight: bold;
-	width:100px;
-	height:30px;
-	border-radius: 15px;
-
+  /* change bg color to get different hues    */
+  background-color: salmon;
+  color: white;
+  /* padding: 2em 3em; */
+  border: none;
+  transition: all 0.3s ease;
+  border-radius: 5px;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+  outline: none;
+  align-self: center;
+  cursor: pointer;
+  font-weight: bold;
+  width: 100px;
+  height: 30px;
+  border-radius: 15px;
 }
 
 .fun-btn:hover {
-	animation: random-bg .5s linear infinite, grow 1300ms ease infinite;
+  animation: random-bg 0.5s linear infinite, grow 1300ms ease infinite;
 }
 
 .start-fun {
-	background-color: #fff !important;
-	/* change color of button text when fun is started   */
-	color: salmon !important;
+  background-color: #fff !important;
+  /* change color of button text when fun is started   */
+  color: salmon !important;
 }
 
 /* pulsating effect on button */
 @keyframes grow {
-	0% {
-		transform: scale(1);
-	}
-	14% {
-		transform: scale(1.3);
-	}
-	28% {
-		transform: scale(1);
-	}
-	42% {
-		transform: scale(1.3);
-	}
-	70% {
-		transform: scale(1);
-	}
+  0% {
+    transform: scale(1);
+  }
+  14% {
+    transform: scale(1.3);
+  }
+  28% {
+    transform: scale(1);
+  }
+  42% {
+    transform: scale(1.3);
+  }
+  70% {
+    transform: scale(1);
+  }
 }
 
 /* 작성하기 버튼 */
 .w-btn {
-		position: relative;
-		border: none;
-		display: inline-block;
-		/* padding: 15px 30px; */
-		border-radius: 15px;
-		font-family: "paybooc-Light", sans-serif;
-		box-shadow: 0 15px 35px rgba(0, 0, 0, 0.2);
-		text-decoration: none;
-		font-weight: 600;
-		transition: 0.25s;
+  position: relative;
+  border: none;
+  display: inline-block;
+  /* padding: 15px 30px; */
+  border-radius: 15px;
+  font-family: "paybooc-Light", sans-serif;
+  box-shadow: 0 15px 35px rgba(0, 0, 0, 0.2);
+  text-decoration: none;
+  font-weight: 600;
+  transition: 0.25s;
 }
 .w-btn-blue {
-		background-color: #6aafe6;
-		color: #d4dfe6;
+  background-color: #6aafe6;
+  color: #d4dfe6;
 }
 </style>
--- final-pjt-front/src/router/index.js
+++ final-pjt-front/src/router/index.js
@@ -1,89 +1,88 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-import SignUp from '@/components/SignUp'
-import LogIn from '@/components/LogIn'
-import PostReview from '@/components/PostReview'
-import ReviewDetail from '@/components/ReviewDetail'
-import EditReview from '@/components/EditReview'
+import SignUp from "@/components/SignUp";
+import LogIn from "@/components/LogIn";
+import PostReview from "@/components/PostReview";
+import ReviewDetail from "@/components/ReviewDetail";
+// import EditReview from "@/components/EditReview";
 
-import HomeView from '@/views/HomeView'
-import testView from '@/views/testView'
-import EditProfileView from '@/views/EditProfileView'
-import RecomView from '@/views/RecomView'
-import ProfileView from '@/views/ProfileView'
-import CommunityView from '@/views/CommunityView'
+import HomeView from "@/views/HomeView";
+import testView from "@/views/testView";
+import EditProfileView from "@/views/EditProfileView";
+import RecomView from "@/views/RecomView";
+import ProfileView from "@/views/ProfileView";
+import CommunityView from "@/views/CommunityView";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 // const isAuth = localStorage.getItem('token')
 
 const routes = [
   {
-    path: '/',
-    name: 'LogIn',
+    path: "/",
+    name: "LogIn",
     component: LogIn,
-
   },
   {
-    path: '/signup',
-    name: 'SignUp',
-    component: SignUp
+    path: "/signup",
+    name: "SignUp",
+    component: SignUp,
   },
   {
-    path: '/home',
-    name: 'HomeView',
-    component: HomeView
+    path: "/home",
+    name: "HomeView",
+    component: HomeView,
   },
   {
-    path: '/profile/:username',
-    name: 'ProfileView',
-    component: ProfileView
+    path: "/profile/:username",
+    name: "ProfileView",
+    component: ProfileView,
   },
   {
-    path: '/recommendation',
-    name: 'RecomView',
+    path: "/recommendation",
+    name: "RecomView",
     component: RecomView,
   },
   {
-    path: '/test',
-    name: 'testView',
-    component: testView
+    path: "/test",
+    name: "testView",
+    component: testView,
   },
   {
-    path: '/edit/:username',
-    name: 'EditProfileView',
+    path: "/edit/:username",
+    name: "EditProfileView",
     component: EditProfileView,
   },
   {
-    path: '/community',
-    name: 'CommunityView',
-    component: CommunityView
+    path: "/community",
+    name: "CommunityView",
+    component: CommunityView,
   },
   {
-    path: '/post/review/:movie_id',
-    name: 'PostReview',
-    component: PostReview
+    path: "/post/review/:movie_id",
+    name: "PostReview",
+    component: PostReview,
   },
+  // {
+  //   path: "/Edit/review/:review_id",
+  //   name: "EditReview",
+  //   component: EditReview,
+  // },
   {
-    path: '/Edit/review/:review_id',
-    name: 'EditReview',
-    component: EditReview
+    path: "/review/:review_id/detail/",
+    name: "ReviewDetail",
+    component: ReviewDetail,
   },
-  {
-    path: '/review/:review_id/detail/',
-    name: 'ReviewDetail',
-    component: ReviewDetail
-  },
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-// router.beforeEach((to,from,next)=>{  
+// router.beforeEach((to,from,next)=>{
 
 //   const allowAllpages = ['LogIn','SignUp']
 
@@ -105,9 +104,9 @@ const router = new VueRouter({
 // https://velog.io/@hschoi1104/Vue.js-NavigationDuplicated-%EC%97%90%EB%9F%AC-%ED%95%B4%EA%B2%B0%ED%95%98%EA%B8%B0
 const originalPush = VueRouter.prototype.push;
 VueRouter.prototype.push = function push(location) {
-	return originalPush.call(this, location).catch(err => {
-		if (err.name !== 'NavigationDuplicated') throw err;
-	});
+  return originalPush.call(this, location).catch((err) => {
+    if (err.name !== "NavigationDuplicated") throw err;
+  });
 };
 
-export default router
+export default router;
